eab927ccef53e014b8357b39b1be8dc7be4d8271
ishidao0910 <goo.ishidao@gmail.com>
2022-07-06 15:21:36 +0900

    initialize project1 for connecting mysql container with go container

---
+++ project01/src/sample/model/db.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var err error
+
+func init() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/line_analysis?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(dsn + "database can't connect")
+	}
+	DB.AutoMigrate(&User{})
+}
