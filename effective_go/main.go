e09891f31a99e7aa8e943b9ece4991820e972f54
ishidao0910 <goo.ishidao@gmail.com>
2022-07-13 01:02:34 +0900

    variables

--- effective_go/main.go
+++ effective_go/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
+// hello world
 	fmt.Println("hello world")
+
 	
+// value
     fmt.Println("go" + "lang")
 
     fmt.Println("1+1 =", 1+1)
@@ -13,4 +16,21 @@ func main() {
     fmt.Println(true && false)
     fmt.Println(true || false)
     fmt.Println(!true)
+
+
+//variables
+    var a = "initial"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+
+    f := "apple"
+    fmt.Println(f)
 }
