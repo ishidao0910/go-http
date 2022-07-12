4f786e1aa2a204fdbc9ccc9f29df8e454c99152d
ishidao0910 <goo.ishidao@gmail.com>
2022-07-13 01:14:21 +0900

    constant

--- effective_go/main.go
+++ effective_go/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
 
 func main() {
 // hello world
@@ -25,12 +30,24 @@ func main() {
     var b, c int = 1, 2
     fmt.Println(b, c)
 
-    var d = true
-    fmt.Println(d)
+    // var d = true
+    // fmt.Println(d)
 
     var e int
     fmt.Println(e)
 
     f := "apple"
     fmt.Println(f)
+
+// constant
+	fmt.Println(s)
+
+    const n = 500000000
+
+    const d = 3e20 / n
+    fmt.Println(d)
+
+    fmt.Println(int64(d))
+
+    fmt.Println(math.Sin(n))
 }
