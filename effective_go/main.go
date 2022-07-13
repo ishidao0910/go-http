410d60caa261ece4536a22afe397502d42c8291e
ishidao0910 <goo.ishidao@gmail.com>
2022-07-13 14:10:49 +0900

    funciton

--- effective_go/main.go
+++ effective_go/main.go
@@ -2,141 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
+func plus(a int, b int) int {
+	return a + b
+}
 
-const s string = "constant"
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
 
 func main() {
-/*
+    res := plus(1, 2)
+   	fmt.Println("1+2 =", res)
 
-// hello world
-	fmt.Println("hello world")
-
-	
-// value
-    fmt.Println("go" + "lang")
-
-    fmt.Println("1+1 =", 1+1)
-    fmt.Println("7.0/3.0 =", 7.0/3.0)
-
-    fmt.Println(true && false)
-    fmt.Println(true || false)
-    fmt.Println(!true)
-
-
-//variables
-    var a = "initial"
-    fmt.Println(a)
-
-    var b, c int = 1, 2
-    fmt.Println(b, c)
-
-    // var d = true
-    // fmt.Println(d)
-
-    var e int
-    fmt.Println(e)
-
-    f := "apple"
-    fmt.Println(f)
-
-// constant
-	fmt.Println(s)
-
-    const n = 500000000
-
-    const d = 3e20 / n
-    fmt.Println(d)
-
-    fmt.Println(int64(d))
-
-    fmt.Println(math.Sin(n))
-
-// for
-	i := 1
-	for i <= 3 {
-		fmt.Println(i)
-		i = i + 1 
-	}
-
-	for j := 7; j <= 9; j++ {
-        fmt.Println(j)
-    }
-
-    for {
-        fmt.Println("loop")
-        break
-    }
-
-    for n := 0; n <= 5; n++ {
-        if n%2 == 0 {
-            continue
-        }
-        fmt.Println(n)
-	}
-
-
-// if
-	if 7%2 == 0 {
-        fmt.Println("7 is even")
-    } else {
-        fmt.Println("7 is odd")
-    }
-
-    if 8%4 == 0 {
-        fmt.Println("8 is divisible by 4")
-    }
-
-    if num := 9; num < 0 {
-        fmt.Println(num, "is negative")
-    } else if num < 10 {
-        fmt.Println(num, "has 1 digit")
-    } else {
-        fmt.Println(num, "has multiple digits")
-    }
-*/
-
-// switch
-
-	i := 2
-    fmt.Print("Write ", i, " as ")
-    switch i {
-    case 1:
-        fmt.Println("one")
-    case 2:
-        fmt.Println("two")
-    case 3:
-        fmt.Println("three")
-    }
-
-    switch time.Now().Weekday() {
-    case time.Saturday, time.Sunday:
-        fmt.Println("It's the weekend")
-    default:
-        fmt.Println("It's a weekday")
-    }
-
-    t := time.Now()
-    switch {
-    case t.Hour() < 12:
-        fmt.Println("It's before noon")
-    default:
-        fmt.Println("It's after noon")
-    }
-
-    whatAmI := func(i interface{}) {
-        switch t := i.(type) {
-        case bool:
-            fmt.Println("I'm a bool")
-        case int:
-            fmt.Println("I'm an int")
-        default:
-            fmt.Printf("Don't know type %T\n", t)
-        }
-    }
-    whatAmI(true)
-    whatAmI(1)
-    whatAmI("hey")
-
+   	res = plusPlus(1, 2, 3)
+   	fmt.Println("1+2+3 =", res)
 }
+
